5a603c6b012f134693018e06833c21cc869e6bbd
Catalin-Ion Besleaga <c_beslea@live.concordia.ca>
2026-06-04 14:29:55 -0400

    Add: search-2d-matrix - submission-18

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-18.java
@@ -0,0 +1,18 @@
+class Solution {
+    //O(n * m) complexity -- Runtime: 154ms ; Memory: 62.8MB
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = 0, c = 0;
+        return backtrack(r, c, matrix, target);
+    }
+
+    public boolean backtrack(int r, int c, int[][] matrix, int target) {
+        if (r >= matrix.length) return false;
+        if (c >= matrix[0].length) { 
+            r++;
+            c = 0;
+            return backtrack(r, c, matrix, target);
+        }
+        if (matrix[r][c] == target) return true;
+        return backtrack(r, ++c, matrix, target);
+    }
+}
