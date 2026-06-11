a9b932563f93322d866d765733a6aceb48bab9ec
Catalin-Ion Besleaga <c_beslea@live.concordia.ca>
2026-06-11 18:51:18 -0400

    Add: products-of-array-discluding-self - submission-14

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-14.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+
+        int ans = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                left[i] = ans;
+                continue;
+            } 
+            ans *= nums[i - 1];
+            left[i] = ans;
+        }
+
+        ans = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i == nums.length - 1) {
+                right[i] = ans;
+                continue;
+            } 
+            ans *= nums[i + 1];
+            right[i] = ans;
+        }
+
+        int[] ansA = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ansA[i] = left[i] * right[i];
+        }
+        return ansA;
+    }
+}  
